a6c6d92688d75df33aa8f3700db9ca330cc07954
yzemaze <dev@yzemaze.de>
2024-12-19 14:12:29 +0100

    paste csv to clipboard; JS beautify

--- replay-to-top-carcassonner.user.js
+++ replay-to-top-carcassonner.user.js
@@ -12,83 +12,84 @@
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_openInTab
-// @version     0.2.2
+// @grant       GM_setClipboard
+// @version     0.2.3
 // @author      yzemaze
 // ==/UserScript==
 
 (function() {
-		'use strict';
-		if (window.location.href.includes('boardgamearena.com/archive/replay')) {
-				if (document.getElementsByClassName("bgagame-carcassonne").length > 0) {
-						addButton();
-				}
-		} else if (window.location.href === 'https://top-carcassonner.com/problems/propose') {
-				document.addEventListener('DOMContentLoaded', function() {
-						setTimeout(function() {
-								pasteData();
-						}, 3000);
-				});
-		}
+    'use strict';
+    if (window.location.href.includes('boardgamearena.com/archive/replay')) {
+        if (document.getElementsByClassName("bgagame-carcassonne").length > 0) {
+            addButton();
+        }
+    } else if (window.location.href === 'https://top-carcassonner.com/problems/propose') {
+        document.addEventListener('DOMContentLoaded', function() {
+            setTimeout(function() {
+                pasteData();
+            }, 3000);
+        });
+    }
 
-		function copyData() {
-				const tableEl = document.getElementById('table_ref_item_table_id');
-				const tableId = tableEl ? tableEl.textContent.replace(/\D/g, "") : ''
-				const remainingTiles = document.getElementById('remainingtiles').textContent.trim();
-				const moveNbr = document.getElementById('move_nbr').textContent.trim();
-				const pageUrl = new URL(window.location);
-				pageUrl.searchParams.set('goto', moveNbr);
-				const tableUrl = "https://boardgamearena.com/gamereview?table=" + tableId;
-				const players = document.querySelectorAll("#player_boards .player-name");
-				const player1 = players[0].textContent.trim().replace(/»\s*/g, "");
-				const player2 = players[1].textContent.trim().replace(/»\s*/g, "");
+    function copyData() {
+        const tableEl = document.getElementById('table_ref_item_table_id');
+        const tableId = tableEl ? tableEl.textContent.replace(/\D/g, "") : ''
+        const remainingTiles = document.getElementById('remainingtiles').textContent.trim();
+        const moveNbr = document.getElementById('move_nbr').textContent.trim();
+        const pageUrl = new URL(window.location);
+        pageUrl.searchParams.set('goto', moveNbr);
+        const tableUrl = "https://boardgamearena.com/gamereview?table=" + tableId;
+        const players = document.querySelectorAll("#player_boards .player-name");
+        const player1 = players[0].textContent.trim().replace(/»\s*/g, "");
+        const player2 = players[1].textContent.trim().replace(/»\s*/g, "");
 
-				GM_setValue('table_ref', tableId);
-				GM_setValue('remaining_tiles', remainingTiles);
-				GM_setValue('page_url', pageUrl.href + "\n\n");
-				GM_setValue('csv', [tableId, moveNbr, remainingTiles, tableUrl, pageUrl.href, player1, player2].join(";"));
+        GM_setValue('table_ref', tableId);
+        GM_setValue('remaining_tiles', remainingTiles);
+        GM_setValue('page_url', pageUrl.href + "\n\n");
+        GM_setClipboard([tableId, moveNbr, remainingTiles, tableUrl, pageUrl.href, player1, player2].join(";"));
 
+        GM_openInTab('https://top-carcassonner.com/problems/propose', {
+            active: true
+        });
+    }
 
-				GM_openInTab('https://top-carcassonner.com/problems/propose', { active: true });
-		}
+    function pasteData() {
+        const tableRefItem = GM_getValue('table_ref', '');
+        const remainingTiles = GM_getValue('remaining_tiles', '');
+        const pageUrl = GM_getValue('page_url', '');
 
-		function pasteData() {
-				const tableRefItem = GM_getValue('table_ref', '');
-				const remainingTiles = GM_getValue('remaining_tiles', '');
-				const pageUrl = GM_getValue('page_url', '');
-				const csv = GM_getValue('csv', '');
+        const tableId = document.getElementById('inline-table-id');
+        const remainingTileCount = document.getElementById('inline-remaining-tile-count');
+        const textArea = document.querySelector('textarea#inline-remaining-tile-count');
 
-				const tableId = document.getElementById('inline-table-id');
-				const remainingTileCount = document.getElementById('inline-remaining-tile-count');
-				const textArea = document.querySelector('textarea#inline-remaining-tile-count');
+        if (tableId) tableId.value = tableRefItem;
+        if (remainingTileCount) remainingTileCount.value = remainingTiles;
+        if (textArea) {
+            textArea.value = pageUrl;
+            textArea.focus();
+        }
+    }
 
-				if (tableId) tableId.value = tableRefItem;
-				if (remainingTileCount) remainingTileCount.value = remainingTiles;
-				if (textArea) {
-						textArea.value = pageUrl + csv;
-						textArea.focus();
-				}
-		}
+    function addButton() {
+        const button = document.createElement('button');
+        button.textContent = 'Copy Data to top-carcassonner';
+        button.style.position = 'fixed';
+        button.style.top = '10px';
+        button.style.left = '180px';
+        button.style.padding = '10px 20px';
+        button.style.width = 'max-content';
+        button.style.backgroundColor = '#4CAF50';
+        button.style.color = 'white';
+        button.style.fontSize = '16px';
+        button.style.border = 'none';
+        button.style.borderRadius = '5px';
+        button.style.cursor = 'pointer';
+        button.style.zIndex = '9999';
 
-		function addButton() {
-				const button = document.createElement('button');
-				button.textContent = 'Copy Data to top-carcassonner';
-				button.style.position = 'fixed';
-				button.style.top = '10px';
-				button.style.left = '180px';
-				button.style.padding = '10px 20px';
-				button.style.width = 'max-content';
-				button.style.backgroundColor = '#4CAF50';
-				button.style.color = 'white';
-				button.style.fontSize = '16px';
-				button.style.border = 'none';
-				button.style.borderRadius = '5px';
-				button.style.cursor = 'pointer';
-				button.style.zIndex = '9999';
+        button.addEventListener('click', function() {
+            copyData();
+        });
 
-				button.addEventListener('click', function() {
-						copyData();
-				});
-
-				document.body.appendChild(button);
-		}
+        document.body.appendChild(button);
+    }
 })();
